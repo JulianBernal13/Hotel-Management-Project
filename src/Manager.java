9fa54dd23529f4bd0effe9f01aa70203efb21aca
juliangarcia13 <37643349+juliangarcia13@users.noreply.github.com>
2019-10-22 16:14:33 -0500

    Update Manager.java

--- src/Manager.java
+++ src/Manager.java
@@ -1,1 +1,11 @@
+package [insert name here];
 
+public class Manager extends Employee {
+	private static String titleName;
+	private static String id;
+	private static int salary;
+
+	public Manager() {
+		super(titleName, id, salary);
+	}
+}
