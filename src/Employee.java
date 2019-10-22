80149facd97ae58164f5a9447f1c8af1fc9f930e
juliangarcia13 <37643349+juliangarcia13@users.noreply.github.com>
2019-10-22 16:39:22 -0500

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,19 +1,34 @@
 package [insert name here]
 /**
  * 
- * @author 
+ * @author
  *
  */
 public class Employee {
 
+	// Specifies the type of Employee (e.g Manager, Cleaner)
 	private String titleName;
+
+	// Specifies the exact person within a position (e.g Cleaner, id:1234 -->
+	// person named "Bob")
 	private String id;
+
+	// Specifies the salary this type of employee gets
 	private int salary;
 
+	// private Employee[] a;
+
+	/**
+	 * Constructs an employee
+	 * 
+	 * @param titleName
+	 * @param id
+	 * @param salary
+	 */
 	public Employee(String titleName, String id, int salary) {
 		this.titleName = titleName;
 		this.id = id;
 		this.salary = salary;
 	}
-	
+
 }
