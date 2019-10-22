df0377e42a17e861c28b67c1bfc4486780e9b895
juliangarcia13 <37643349+juliangarcia13@users.noreply.github.com>
2019-10-22 16:59:32 -0500

    Update Manager.java

--- src/Manager.java
+++ src/Manager.java
@@ -1,9 +1,23 @@
 package [insert name here]
 
 public class Manager extends Employee {
+	private String id;
+	private int salary;
 
-	public Manager() {
-		super("Manager", "1234", 70000);
+	public Manager(String titleName, String id, int salary) {
+		super("Manager");
+		setEmployeeID(id);
+		setSalary(salary);
+	}
+
+	@Override
+	public void setEmployeeID(String id) {
+		this.id = id;
+	}
+
+	@Override
+	public void setSalary(int salary) {
+		this.salary = salary;
 	}
 
 	public void deleteEmployee(Employee[] a, String titleName, String id) {
