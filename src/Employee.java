bd4b994896fca8a446c2a451022801f5101dc4b3
juliangarcia13 <37643349+juliangarcia13@users.noreply.github.com>
2019-10-22 16:14:00 -0500

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,1 +1,20 @@
+package [insert name here];
 
+/**
+ * 
+ * @author 
+ *
+ */
+public class Employee {
+
+	private String titleName;
+	private String id;
+	private int salary;
+
+	public Employee(String titleName, String id, int salary) {
+		this.titleName = titleName;
+		this.id = id;
+		this.salary = salary;
+	}
+
+}
