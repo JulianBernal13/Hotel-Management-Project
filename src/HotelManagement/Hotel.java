1a7e5df47e890cdcbf04eca571505ed4a1230404
Eenhuis <jeenhuis@iastate.edu>
2019-10-24 16:55:42 -0500

    added some imports to main for IO

--- src/HotelManagement/Hotel.java
+++ src/HotelManagement/Hotel.java
@@ -2,6 +2,9 @@ package HotelManagement;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * @author Yingxie Gao
