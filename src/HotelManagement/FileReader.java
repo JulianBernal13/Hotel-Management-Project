5e92213e3e06fafe5d4562ab9fb22b22c67b3e47
juliangarcia13 <37643349+juliangarcia13@users.noreply.github.com>
2019-10-29 20:42:45 -0500

    Update FileReader.java

--- src/HotelManagement/FileReader.java
+++ src/HotelManagement/FileReader.java
@@ -5,28 +5,33 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class FileReader {
-    public static String getHotelInfo(File file, Hotel.HotelProperty property) throws FileNotFoundException {
-        Scanner in = new Scanner(file);
-        int i = 0;
-        while(i++ < property.ordinal() && in.hasNext())
-            in.nextLine();
-        return in.nextLine();
-    }
+	public static String getHotelInfo(File file, Hotel.HotelProperty property) throws FileNotFoundException {
+		Scanner in = new Scanner(file);
+		int i = 0;
+		while (i++ < property.ordinal() && in.hasNext())
+			in.nextLine();
+		return in.nextLine();
+	}
 
-    public static void displayHotelInfo(File file) throws FileNotFoundException {
-        Scanner in = new Scanner(file);
-        for (Hotel.HotelProperty tmp : Hotel.HotelProperty.values()) {
-            System.out.println(tmp.name() + ": " + in.nextLine());
-        }
-    }
-    
-    public static void displayRoomInfo(File file) throws FileNotFoundException {
-    	Scanner in = new Scanner(file);
-    	for (Room.RoomProperty tmp : Room.RoomProperty.values()) {
-            System.out.println(tmp.name() + ": " + in.nextLine());
-        }
-    }
+	public static void displayHotelInfo(File file) throws FileNotFoundException {
+		Scanner in = new Scanner(file);
+		for (Hotel.HotelProperty tmp : Hotel.HotelProperty.values()) {
+			System.out.println(tmp.name() + ": " + in.nextLine());
+		}
+	}
 
-    public static void displayEmpINfo(File info) throws FileNotFoundException{
-    }
+	public static void displayRoomInfo(File file) throws FileNotFoundException {
+		Scanner in = new Scanner(file);
+		for (Room.RoomProperty tmp : Room.RoomProperty.values()) {
+			System.out.println(tmp.name() + ": " + in.nextLine());
+		}
+	}
+
+	public static void displayEmpInfo(File file) throws FileNotFoundException {
+		Scanner in = new Scanner(file);
+		for (Employee.EmployeeProperty tmp : Employee.EmployeeProperty.values()) {
+			System.out.println(tmp.name() + ": " + in.nextLine());
+		}
+	}
+
 }
