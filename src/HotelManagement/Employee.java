87d8a25e422741e6a68b8647efdc9137f1f1d25d
juliangarcia13 <37643349+juliangarcia13@users.noreply.github.com>
2019-10-30 13:22:28 -0500

    Update Employee.java

--- src/HotelManagement/Employee.java
+++ src/HotelManagement/Employee.java
@@ -1,5 +1,7 @@
 package HotelManagement;
 
+import java.io.File;
+
 /**
  * 
  * @author Julian Garcia
@@ -10,13 +12,19 @@ public class Employee {
 	// Specifies the type of HotelManagement.Employee (e.g
 	// HotelManagement.Manager, Cleaner)
 	private String titleName;
-	
-	//ID (e.g. C43245)
+
+	// ID (e.g. C43245)
 	private String id;
-	
+
 	private String paymentType;
-	private int wage;
-	
+	private int salary;
+	private String path;
+	private String hotel;
+	private boolean clockIn;
+
+	public static enum EmployeeProperty {
+		titleName, id, paymentType, salary
+	}
 	// private HotelManagement.Employee[] a;
 
 	/**
@@ -24,137 +32,73 @@ public class Employee {
 	 * 
 	 * @param titleName
 	 */
-	public Employee(String titleName, String id, String paymentType, int wage) {
+	public Employee(String path, String titleName, String id, String paymentType, int salary) {
+		this.path = path;
 		this.titleName = titleName;
 		this.id = id;
 		this.paymentType = paymentType;
-		this.wage = wage;
-	}
-
-	// OVERRIDE IN SUBCLASSES
-	public void setEmployeeID(String id) {
-
-	}
-
-	// OVERRIDE IN SUBCLASSES
-	public void setSalary(int salary) {
-
-	}
-
-	public String getTitleName() {
-		return titleName;
+		this.salary = salary;
+		clockIn = false;
+		hotel = checkCurrentHotel();
+		// this.path = path + File.separator + id + ".txt";;
 	}
 
 	public void setTitleName(String titleName) {
 		this.titleName = titleName;
 	}
 
-	public String getID() {
-		return id;
-	}
-
 	public void setID(String id) {
 		this.id = id;
 	}
 
-	/**
-	 * A=10, B=11, C=12, .... , Z=35
-	 * 
-	 * Employee has "CXXX" with C indicating its titleName (e.g. Cleaner). True ID looks like
-	 * "12"
-	 * 
-	 * (e.g ACXXX=1012 , MXXXXX=22 , etc.)
-	 * 
-	 * (This method is used to sort employees alphabet style)
-	 * 
-	 * @return alphabet
-	 */
-	public String getTrueID(String id) {
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
 
-		String trueID = "";
-		for (int i = 0; i < id.length(); i++) {
-			if (Character.isLetter(id.charAt(i)) == true) {
-				if (id.charAt(i) == 'A') {
-					trueID += "10";
-				}
-				if (id.charAt(i) == 'B') {
-					trueID += "11";
-				}
-				if (id.charAt(i) == 'C') {
-					trueID += "12";
-				}
-				if (id.charAt(i) == 'D') {
-					trueID += "13";
-				}
-				if (id.charAt(i) == 'E') {
-					trueID += "14";
-				}
-				if (id.charAt(i) == 'F') {
-					trueID += "15";
-				}
-				if (id.charAt(i) == 'G') {
-					trueID += "16";
-				}
-				if (id.charAt(i) == 'H') {
-					trueID += "17";
-				}
-				if (id.charAt(i) == 'I') {
-					trueID += "18";
-				}
-				if (id.charAt(i) == 'J') {
-					trueID += "19";
-				}
-				if (id.charAt(i) == 'K') {
-					trueID += "20";
-				}
-				if (id.charAt(i) == 'L') {
-					trueID += "21";
-				}
-				if (id.charAt(i) == 'M') {
-					trueID += "22";
-				}
-				if (id.charAt(i) == 'N') {
-					trueID += "23";
-				}
-				if (id.charAt(i) == 'O') {
-					trueID += "24";
-				}
-				if (id.charAt(i) == 'P') {
-					trueID += "25";
-				}
-				if (id.charAt(i) == 'Q') {
-					trueID += "26";
-				}
-				if (id.charAt(i) == 'R') {
-					trueID += "27";
-				}
-				if (id.charAt(i) == 'S') {
-					trueID += "28";
-				}
-				if (id.charAt(i) == 'T') {
-					trueID += "29";
-				}
-				if (id.charAt(i) == 'U') {
-					trueID += "30";
-				}
-				if (id.charAt(i) == 'V') {
-					trueID += "31";
-				}
-				if (id.charAt(i) == 'W') {
-					trueID += "32";
-				}
-				if (id.charAt(i) == 'X') {
-					trueID += "33";
-				}
-				if (id.charAt(i) == 'Y') {
-					trueID += "34";
-				}
-				if (id.charAt(i) == 'Z') {
-					trueID += "35";
-				}
-			}
-		}
-		return trueID;
+	public void setPaymentType(String paymentType) {
+		this.paymentType = paymentType;
+	}
+
+	public void setHotel(String hotel) {
+		this.hotel = hotel;
+	}
+
+	public String getHotel(String hotel) {
+		return this.hotel;
+	}
+
+	// Retrieves the hotel from the path
+	public String checkCurrentHotel() {
+		String hotel = path.substring(19, path.length() - 9);
+		return hotel;
+	}
+
+	public void clockIn() {
+		clockIn = true;
+	}
+
+	public void clockOut() {
+		clockIn = false;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public String getTitleName() {
+		return titleName;
+	}
+
+	public String getID() {
+		return id;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public String getPaymentType() {
+		return paymentType;
 	}
 
 	public String toString() {
