7508866b878950051a26508195022765a588209c
juliangarcia13 <37643349+juliangarcia13@users.noreply.github.com>
2019-10-31 09:08:28 -0500

    Not yet working

--- src/HotelManagement/EmployeeFileController.java
+++ src/HotelManagement/EmployeeFileController.java
@@ -13,31 +13,31 @@ public class EmployeeFileController implements FileController {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
         registerEmployee(employees, name);
-        getCustomerInfo(employees, name, "isVIP");
-        getCustomerInfo(employees, name, Customer.customerProperty.firstname);
+        getEmployeeInfo(employees, name, "isVIP");
+        getEmployeeInfo(employees, name, Employee.EmployeeProperty.titleName);
 
     }
-    public static void displayAll(File customers) {
+    public static void displayAll(File employees) {
 
     }
 
-    public static File lookUpCustomer(File employees, String id) {
+    public static File lookUpEmployee(File employees, String id) {
         return new File(employees.getPath() + File.separator + FileController.convertToTxt(id));
     }
 
     public static File registerEmployee(File employees, String name) throws IOException {
-        if(!lookUpCustomer(employees, name).exists())
+        if(!lookUpEmployee(employees, name).exists())
             return createEmployee(employees, name);
-        System.out.println("Error, the customer has already existed");
+        System.out.println("Error, the employee has already existed");
         return null;
     }
 
-    public static File createEmployee(File customers, String name) throws IOException {
+    public static File createEmployee(File employees, String name) throws IOException {
         int i = name.indexOf(' ');
         String firstName = name.substring(0, i), lastName = name.substring(i + 1);
-        String filePath = customers.getPath() + File.separator + FileController.convertToTxt(name);
-        File newCustomer = new File(filePath);
-        if(newCustomer.createNewFile()) {
+        String filePath = employees.getPath() + File.separator + FileController.convertToTxt(name);
+        File newEmployee = new File(filePath);
+        if(newEmployee.createNewFile()) {
             PrintWriter writer = new PrintWriter(filePath);
             writer.println(firstName);
             writer.println(lastName);
@@ -46,7 +46,7 @@ public class EmployeeFileController implements FileController {
             writer.flush();
             writer.close();
         }
-        return newCustomer;
+        return newEmployee;
     }
 
     public static int getPropertyOrdinal(String property) {
@@ -59,13 +59,13 @@ public class EmployeeFileController implements FileController {
         return -1;
     }
 
-    public static void getCustomerInfo(File customers, String name, Customer.customerProperty property) throws FileNotFoundException {
-        File customer = lookUpCustomer(customers, name);
-        if(!customer.exists()) {
-            System.out.println("Customer dose not exists");
+    public static void getEmployeeInfo(File employees, String name, Employee.EmployeeProperty property) throws FileNotFoundException {
+        File employee = lookUpEmployee(employees, name);
+        if(!employee.exists()) {
+            System.out.println("Employee dose not exists");
             return;
         }
-        Scanner sc = new Scanner(customer);
+        Scanner sc = new Scanner(employee);
         int i = 0;
         while(i++ < property.ordinal() && sc.hasNext())
             sc.nextLine();
@@ -75,14 +75,14 @@ public class EmployeeFileController implements FileController {
         System.out.println(ret);
     }
 
-    public static void getCustomerInfo(File customers, String name, String property) throws FileNotFoundException {
-        File customer = lookUpCustomer(customers, name);
-        if(!customer.exists()) {
-            System.out.println("Customer dose not exists");
+    public static void getEmployeeInfo(File employees, String name, String property) throws FileNotFoundException {
+        File employee = lookUpEmployee(employees, name);
+        if(!employee.exists()) {
+            System.out.println("Employee does not exists");
             return;
         }
-        Scanner sc = new Scanner(customer);
-        for(Customer.customerProperty p : Customer.customerProperty.values()) {
+        Scanner sc = new Scanner(employee);
+        for(Employee.EmployeeProperty p : Employee.EmployeeProperty.values()) {
             if(p.toString() == property)
                 break;
             sc.nextLine();
@@ -95,23 +95,23 @@ public class EmployeeFileController implements FileController {
         System.out.println(ret);
     }
 
-    public static File cdCustomerFile(File hotel) {
-        String filePath = hotel.getPath() + File.separator + "Customer";
+    public static File cdEmployeeFile(File hotel) {
+        String filePath = hotel.getPath() + File.separator + "Employee";
         return new File(filePath);
     }
 
-    public static ArrayList<String> extractCustmorInfo(File customers, String name) throws FileNotFoundException {
+    public static ArrayList<String> extractEmpInfo(File employees, String name) throws FileNotFoundException {
         ArrayList<String> info = new ArrayList<>();
-//        File customer = lookUpCustomer(customers, name);
-        Scanner sc = new Scanner(lookUpCustomer(customers, name));
+//        File employee = lookUpEmployee(employee, name);
+        Scanner sc = new Scanner(lookUpEmployee(employees, name));
         while(sc.hasNext()) {
             info.add(sc.nextLine());
         }
         return info;
     }
 
-    public static void modifyCustomer(File customers, String name, String property) throws FileNotFoundException {
-        ArrayList<String> info = extractCustmorInfo(customers, name);
+    public static void modifyEmployee(File employees, String name, String property) throws FileNotFoundException {
+        ArrayList<String> info = extractEmpInfo(employees, name);
         System.out.println("Enter what you like to change to " + name + ": " + property);
         Scanner sc = new Scanner(System.in);
         int i = getPropertyOrdinal(property);
