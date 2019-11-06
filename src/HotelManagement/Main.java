a54b741f2f653d2470d1650bef3b0d8875ad030c
gaoyx <gaoyingxie@gmail.com>
2019-11-05 22:27:39 -0600

    add date to main

--- src/HotelManagement/Main.java
+++ src/HotelManagement/Main.java
@@ -1,6 +1,10 @@
 package HotelManagement;
 
 import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * @author Yingxie Gao
@@ -9,6 +13,14 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        SimpleDateFormat sdf = new SimpleDateFormat ("yyyy-MM-dd");
+        Date current = new Date();
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(current);
+        //add 1 day to calendar
+        calendar.add(Calendar.DATE, 1);
+        current = calendar.getTime();
+        System.out.println(sdf.format(current));
         MainMenu m = new MainMenu();
         m.menu();
     }
