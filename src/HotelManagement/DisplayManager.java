bd03c29ea487078d899dc92b4452ebebcc5c80f9
juliangarcia13 <37643349+juliangarcia13@users.noreply.github.com>
2019-11-28 17:06:34 -0600

    Update DisplayManager.java

--- src/HotelManagement/DisplayManager.java
+++ src/HotelManagement/DisplayManager.java
@@ -1,16 +1,21 @@
 package HotelManagement;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class DisplayManager extends aDisplay {
-    Hotel hotel;
-    Manager manager;
+	Hotel hotel;
+	Manager manager;
 
-
-    public DisplayManager(Hotel hotel, Manager manager) {
-        this.hotel = hotel;
-        this.manager = manager;
-        addCommand(new CommandShowHotelInfo(hotel));
-        addCommand(new CommandEditRoomManager(hotel));
-        addCommand(new CommandEditEmployeeManager(hotel));
-        addCommand(new CommandInventoryManager(hotel));
-    }
+	public DisplayManager(Hotel hotel, Manager manager) throws IOException {
+		this.hotel = hotel;
+		this.manager = manager;
+		addCommand(new CommandShowHotelInfo(hotel));
+		addCommand(new CommandEditRoomManager(hotel));
+		addCommand(new CommandEditEmployeeManager(hotel));
+		addCommand(new CommandInventoryManager(hotel));
+		addCommand(new CommandMove(hotel)); // new code
+		addCommand(new CommandAddRoom(hotel)); // new code
+		addCommand(new CommandDeleteRoom(hotel)); // new code
+	}
 }
