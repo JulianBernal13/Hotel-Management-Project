344a62137cec0eb7f5e6892d8e97221a2b33a18f
juliangarcia13 <37643349+juliangarcia13@users.noreply.github.com>
2019-11-28 17:22:51 -0600

    Moving objs and add/delete room use case

--- src/HotelManagement/DetermineOccupation.java
+++ src/HotelManagement/DetermineOccupation.java
@@ -5,31 +5,39 @@ import java.text.ParseException;
 import java.util.Scanner;
 
 public class DetermineOccupation {
-    public static boolean execute(Hotel hotel) throws IOException, ParseException {
-        Scanner sc = new Scanner(System.in);
+	public static boolean execute(Hotel hotel) throws IOException, ParseException {
+		Scanner sc = new Scanner(System.in);
 
-        System.out.println("Please enter your ID");
-        Printer.printManagerID(hotel);
-        String id = sc.nextLine();
-        if(hotel.getManager(id) != null){
-            DisplayManager displayManager = new DisplayManager(hotel, hotel.getManager());
-            if(displayManager.displayCommand()) {
-                return execute(hotel);
-            }
-            return true;
-        }
-        Employee e = hotel.getEmployee(id);
-        if(e != null) {
-            DisplayReception displayReception = new DisplayReception(hotel, e);
-            if(displayReception.displayCommand()){
-                return execute(hotel);
-            }
-            return true;
-        }
-        System.out.println("ID does not exist, do you want to continue? y/n");
-        if (sc.nextLine().equals("y")) {
-            return execute(hotel);
-        }
-        return true;
-    }
+		System.out.println("Please enter your ID");
+		Printer.printManagerID(hotel);
+		String id = sc.nextLine();
+		if (hotel.getManager(id) != null) {
+			DisplayManager displayManager = new DisplayManager(hotel, hotel.getManager());
+			if (displayManager.displayCommand()) {
+				return execute(hotel);
+			}
+			return true;
+		}
+		Employee e = hotel.getEmployee(id);
+		if (e != null) {
+			DisplayReception displayReception = new DisplayReception(hotel, e);
+			if (displayReception.displayCommand()) {
+				return execute(hotel);
+			}
+			return true;
+		}
+		Customer c = hotel.getCustomer(id);
+		if (c != null) {
+			DisplayCustomer displayCustomer = new DisplayCustomer(hotel, c);
+			if (displayCustomer.displayCommand()) {
+				return execute(hotel);
+			}
+			return true;
+		}
+		System.out.println("ID does not exist, do you want to continue? y/n");
+		if (sc.nextLine().equals("y")) {
+			return execute(hotel);
+		}
+		return true;
+	}
 }
