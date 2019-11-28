129814a9bd7094788209a86708bc916b25bd2d50
juliangarcia13 <37643349+juliangarcia13@users.noreply.github.com>
2019-11-28 17:20:20 -0600

    Moving objs and add/delete room use case

---
+++ src/HotelManagement/CommandCustomerDeleteReservation.java
@@ -0,0 +1,22 @@
+package HotelManagement;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+public class CommandCustomerDeleteReservation extends aCommandCustomer {
+	public CommandCustomerDeleteReservation(Hotel h, Customer c) {
+		hotel = h;
+		customer = c;
+	}
+
+	@Override
+	public boolean execute() throws IOException, ParseException {
+		ContractFileController.deleteContractRes(hotel, customer);
+		return true;
+	}
+
+	@Override
+	public String getDescription() {
+		return "delete reservation";
+	}
+}
