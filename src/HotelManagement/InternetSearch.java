7a990b35b8d9d1ae2dfa6e69439d4359d299468e
Eenhuis <jeenhuis@iastate.edu>
2019-12-10 11:20:31 -0600

    test

--- src/HotelManagement/InternetSearch.java
+++ src/HotelManagement/InternetSearch.java
@@ -1,25 +1,22 @@
 package HotelManagement;
 
-import java.io.BufferedReader;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UncheckedIOException;
 import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLEncoder;
+
 
 public class InternetSearch {
 
-	public static BufferedReader googleSearch(String search) throws IOException {
+	public static Document googleSearch(String search) throws UnsupportedEncodingException, IOException{
 		String googleAJAX = "https://www.google.com/search?q=";
-		String searchfor = search;
-		
-		URL url = new URL(googleAJAX + URLEncoder.encode(searchfor, "UTF-8"));
-		URLConnection urlConn = url.openConnection();
-		InputStreamReader inStream = new InputStreamReader(urlConn.getInputStream());
-		BufferedReader buff = new BufferedReader(inStream);
-		return buff;
+		String searchfor = "hello";
+	
+		Document google = Jsoup.connect(googleAJAX + URLEncoder.encode(searchfor, "UTF-8")).get();
+				
+		return google;
 	}
 }
--- src/HotelManagement/StockSerach.java
+++ src/HotelManagement/StockSerach.java
@@ -4,18 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Scanner;
 
+import org.jsoup.nodes.Document;
+
 public class StockSerach {
 
 	public static void StockLook() throws IOException {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("please enter the name of the bussiness you wish to look up");
 		String input = sc.nextLine();
-		BufferedReader buff = InternetSearch.googleSearch(input);
-		
-		String line = buff.readLine();
-		while(line != null) {
-			System.out.println(line);
-			line = buff.readLine();
-		}
+		Document buff = InternetSearch.googleSearch(input);
+
 	}
 }
